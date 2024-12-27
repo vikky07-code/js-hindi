70add9d70b1f07ebced91719ec234c57f12cdde3
vikky07-code <vikramjeetrasoolpur2004@gmail.com>
2024-12-27 16:03:43 +0000

    strings

---
+++ basics/strings.js
@@ -0,0 +1,24 @@
+
+let Name="vikramjeet"
+
+let rollno=2201430100264
+
+console.log(`my name is ${Name} and my university roll no. is ${rollno}`)
+
+let test="vicky"
+
+console.log(test.toUpperCase());
+console.log(test.toLocaleLowerCase());
+console.log(test.length);
+console.log(test.charAt(2));
+console.log(test.slice(0,3));
+console.log(test.indexOf('y'));
+
+console.log(test.replace('c','k'));
+
+
+
+
+
+
+
