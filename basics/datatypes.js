9e176a67014847c7c234358560aee47776d56abf
vikky07-code <vikramjeetrasoolpur2004@gmail.com>
2024-12-27 14:44:18 +0000

    datatypes

---
+++ basics/datatypes.js
@@ -0,0 +1,8 @@
+
+//data type is divided into 2 category  primitive and non primitive 
+
+//primitive include:-
+// string , number , boolean, null, undefined, bigInt, symbol
+
+//non primitive :-
+//array, objects , functions
