f3372ad6326f004dbf8ca4abe181518e84efdf2d
vikky07-code <vikramjeetrasoolpur2004@gmail.com>
2024-12-27 13:51:42 +0000

    conversion

---
+++ basics/dtypeconversion.js
@@ -0,0 +1,16 @@
+
+const accounttype="savings"
+
+let accountycity="hapur"
+
+var accountstate="UP"
+
+//accounttype="current"; const datatype cannot be change
+
+//console.log(accounttype)
+
+accountycity="faridabad" //these value can be changed in let and var keyword
+
+accountstate="haryana"
+
+console.table([accountstate,accounttype,accountycity])
