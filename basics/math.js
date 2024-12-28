d57750844eef1b39acd84d232603a9ec5e17b935
vikky07-code <vikramjeetrasoolpur2004@gmail.com>
2024-12-28 10:13:30 +0000

    math

---
+++ basics/math.js
@@ -0,0 +1,15 @@
+
+
+let number=9.86;
+
+console.log(number.toPrecision(2));
+
+console.log(Math.max(2,4));
+
+/* to print random number between the min and max value*/
+
+let min=10;
+
+let max=20;
+
+console.log(Math.floor(Math.random()*(max-min+1))+min);
