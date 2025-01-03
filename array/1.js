6630e40489bdf856d53c08c3683dbffe77370664
vikky07-code <vikramjeetrasoolpur2004@gmail.com>
2025-01-03 10:29:03 +0000

    array

---
+++ array/1.js
@@ -0,0 +1,34 @@
+
+/*let myArr=new Array(0,1,2,3)
+
+console.log(myArr[1])
+
+// array methods
+
+myArr.push(6);
+console.log(myArr)
+myArr.reverse()
+
+//console.log(myArr.reverse());
+console.log(myArr)
+
+//convert array to string
+
+
+console.log(myArr.join())*/
+
+let n1=[0,1,2,3,4]
+
+console.log("A",n1);
+
+console.log(n1.slice(1,3));
+console.log("A",n1);
+
+
+console.log(n1.splice(1,3))
+console.log("A",n1);
+
+//in splice the portion is removed from the original array too where as in slice original array remain same
+
+
+
