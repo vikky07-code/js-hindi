a88679b42dec9bdb90f9767cc4cefa588a1fecaa
vikky07-code <vikramjeetrasoolpur2004@gmail.com>
2025-02-09 14:04:47 +0000

    function

---
+++ functions/1.js
@@ -0,0 +1,29 @@
+
+/*function addnumber(num1,num2){
+
+    return num1+num2
+}
+
+console.log(addnumber(4,5));*/
+
+/*function numbers(...num1){
+
+    return num1
+}
+
+console.log(numbers(100,200,300,400));*/
+
+
+const user={
+    name:"vikramjeet",
+    age:21
+}
+
+function objectuser(anyobject){
+
+    console.log(`The name of user is ${anyobject.name} and Age of user is ${anyobject.age}`);
+    
+}
+
+console.log(objectuser(user));
+
